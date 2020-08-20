df97b5672d7c21cfa210e11303584691d07e5d2b
sumitra kumari shaw <sumitrakumarishaw1@gmail.com>
2020-08-20 12:26:04 +0530

    Add Enviroment Vars

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+ 
 
 const app=express();
+
+console.log(process.env.API_KEY);
+
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -19,8 +24,8 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-const secret="thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret:secret, encryptedFields:['password']});
+
+userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields:['password']});
 
 const User=new mongoose.model("User",userSchema);
 
